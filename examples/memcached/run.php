573b36ef1eaa491944cea8a7c242160ffaddf6fc
Daniel Bickett <dbickett@gmail.com>
2013-12-07 21:39:04 -0500

    4 tab spaces

--- examples/memcached/run.php
+++ examples/memcached/run.php
@@ -8,12 +8,12 @@ $loader->add('Directus\\ClosureCache\\', __DIR__ . '/../../src/');
 
 // Initialize Cache interface
 $Cache = new Cache(array(
-	'adapter' => 'memcached',
-	'options' => array(
-		'servers' => array(
-			array('127.0.0.1', 11211)
-		)
-	)
+    'adapter' => 'memcached',
+    'options' => array(
+        'servers' => array(
+            array('127.0.0.1', 11211)
+        )
+    )
 ));
 
 // This defines the random Wikipedia title fetcher:
@@ -22,7 +22,7 @@ require "../slowOperations.php";
 echo "[On the fly]\n";
 
 $articleTitle = $Cache->cache('randomWikipediaArticleTitle', function() {
-	return getRandomWikipediaArticleTitle();
+    return getRandomWikipediaArticleTitle();
 });
 
 echo "Got Wikipedia article title: $articleTitle\n";
@@ -31,7 +31,7 @@ echo "\n";
 echo "[Pre-defined]\n";
 
 $Cache->define('anotherRandomWikipediaArticleTitle', function() {
-	return getRandomWikipediaArticleTitle();
+    return getRandomWikipediaArticleTitle();
 });
 
 $articleTitle = $Cache->warm('anotherRandomWikipediaArticleTitle');
@@ -51,25 +51,25 @@ echo "\n";
 echo "[Pre-defined with arguments]\n";
 
 $Cache->define('listArticleTitleMultipleTimes', function($times) use ($Cache) {
-	$Cache->expire('anotherRandomWikipediaArticleTitle');
-	$titleList = array();
-	while($times--) {
-		$titleList[] = $Cache->warm('anotherRandomWikipediaArticleTitle');
-	}
-	return $titleList;
+    $Cache->expire('anotherRandomWikipediaArticleTitle');
+    $titleList = array();
+    while($times--) {
+        $titleList[] = $Cache->warm('anotherRandomWikipediaArticleTitle');
+    }
+    return $titleList;
 });
 
 echo "Listing random title 5 times:\n";
 $titleList5 = $Cache->warm('listArticleTitleMultipleTimes', array(5));
 foreach($titleList5 as $title) {
-	echo "$title\n";
+    echo "$title\n";
 }
 
 echo "\n";
 echo "Listing random title 10 times:\n";
 $titleList10 = $Cache->warm('listArticleTitleMultipleTimes', array(10));
 foreach($titleList10 as $title) {
-	echo "$title\n";
+    echo "$title\n";
 }
 
 $Cache->expire('listArticleTitleMultipleTimes', array(5));
--- examples/slowOperations.php
+++ examples/slowOperations.php
@@ -1,27 +1,27 @@
 <?php
 
 function getRandomWikipediaArticleTitle() {
-	$context = stream_context_create(
-		array(
-			'http' => array(
-				'follow_location' => false
-			)
-		)
-	);
-	$html = file_get_contents("http://en.wikipedia.org/wiki/Special:Random", false, $context);
-	$wikipediaUrl = false;
-	foreach($http_response_header as $header) {
-		if("Location:" === substr($header, 0, 9)) {
-			$wikipediaUrl = substr($header, 10);
-		}
-	}
-	$articleTitle = "Error";
-	if($wikipediaUrl) {
-		$articleSlugPos = strrpos($wikipediaUrl, '/');
-		if($articleSlugPos) {
-			$articleTitle = substr($wikipediaUrl, $articleSlugPos + 1);
-			$articleTitle = str_replace('_', ' ', urldecode($articleTitle));
-		}
-	}
-	return $articleTitle;
+    $context = stream_context_create(
+        array(
+            'http' => array(
+                'follow_location' => false
+            )
+        )
+    );
+    $html = file_get_contents("http://en.wikipedia.org/wiki/Special:Random", false, $context);
+    $wikipediaUrl = false;
+    foreach($http_response_header as $header) {
+        if("Location:" === substr($header, 0, 9)) {
+            $wikipediaUrl = substr($header, 10);
+        }
+    }
+    $articleTitle = "Error";
+    if($wikipediaUrl) {
+        $articleSlugPos = strrpos($wikipediaUrl, '/');
+        if($articleSlugPos) {
+            $articleTitle = substr($wikipediaUrl, $articleSlugPos + 1);
+            $articleTitle = str_replace('_', ' ', urldecode($articleTitle));
+        }
+    }
+    return $articleTitle;
 }

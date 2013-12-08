c6c9276fb7d8bc50a1a5bec12a4cd440350468bc
Daniel Bickett <dbickett@gmail.com>
2013-12-07 19:37:03 -0500

    docblock annotations

--- src/Directus/ClosureCache/Cache.php
+++ src/Directus/ClosureCache/Cache.php
@@ -9,19 +9,41 @@ use Zend\Cache\StorageFactory;
 class Cache
 {
 
+    /**
+     * @var array
+     */
     protected $operations = array();
 
+    /**
+     * @var Zend\Cache\Storage\StorageInterface
+     */
     protected $storage;
 
+    /**
+     * @param Array $options The options array accepted by Zend\Cache\StorageFactory::factory
+     * See: http://framework.zend.com/manual/2.0/en/modules/zend.cache.storage.adapter.html#quick-start
+     */
     public function __construct(Array $options) {
         $this->storage = StorageFactory::factory($options);
     }
 
+    /**
+     * Cache the result of the $callable parameter under the provided name.
+     * @param  string $name
+     * @param  callable $callable
+     * @return mixed
+     */
     public function cache($name, $callable) {
         $Operation = new Operation($this->storage, $name, $callable);
         return $Operation->getValue();
     }
 
+    /**
+     * Define a cache operation. If the callable accepts arguments, they will be passed via the Cache#warm method.
+     * @param  string $name
+     * @param  callable $callable
+     * @return void
+     */
     public function define($name, $callable) {
         if(isset($this->operations[$name])) {
             throw new CacheOperationNameAlreadyDefinedException("Cache operation with name $name already exists.");
@@ -29,6 +51,12 @@ class Cache
         $this->operations[$name] = new Operation($this->storage, $name, $callable);
     }
 
+    /**
+     * Expire the cache entry for an operation with the given name, optionally with arguments.
+     * @param  string $name
+     * @param  null|array $arguments Must be serializable.
+     * @return bool
+     */
     public function expire($name, $arguments = null) {
         if(!isset($this->operations[$name])) {
             throw new CacheOperationNameUndefinedException("Cache operation with name $name is undefined.");
@@ -36,14 +64,25 @@ class Cache
         return $this->operations[$name]->expire($arguments);
     }
 
+    /**
+     * @return array Array of names of currently defined operations.
+     */
     public function getNames() {
         return array_keys($this->operations);
     }
 
+    /**
+     * @return Zend\Cache\Storage\StorageInterface
+     */
     public function getStorage() {
         return $this->storage;
     }
 
+    /**
+     * Remove an operation definition by name.
+     * @param  string $name The name of the operation to be removed.
+     * @return void
+     */
     public function undefine($name) {
         if(!isset($this->operations[$name])) {
             throw new CacheOperationNameUndefinedException("Cache operation with name $name is undefined.");
@@ -51,6 +90,13 @@ class Cache
         unset($this->operations[$name]);
     }
 
+    /**
+     * Run a pre-defined cache operation by name. Optionally include arguments if the operation definition accepts
+     * them.
+     * @param  string $name
+     * @param  null|array $arguments Must be serializable.
+     * @return mixed The result of the operation, optionally with arguments.
+     */
     public function warm($name, $arguments = null) {
         if(!isset($this->operations[$name])) {
             throw new CacheOperationNameUndefinedException("Cache operation with name $name is undefined.");
--- src/Directus/ClosureCache/Operation.php
+++ src/Directus/ClosureCache/Operation.php
@@ -4,12 +4,32 @@ namespace Directus\ClosureCache;
 
 use Zend\Cache\Storage\StorageInterface;
 
+/**
+ * This class is used internally by Directus\ClosureCache\Cache. A single cacheable operation.
+ */
 class Operation
 {
+
+    /**
+     * @var Zend\Cache\Storage\StorageInterface
+     */
     protected $storage;
+
+    /**
+     * @var string
+     */
     protected $name;
+
+    /**
+     * @var callable
+     */
     protected $callable;
 
+    /**
+     * @param StorageInterface $storage
+     * @param string           $name
+     * @param callable         $callable
+     */
     public function __construct(StorageInterface $storage, $name, $callable) {
         if(!is_callable($callable)) {
             throw new \InvalidArgumentException("Parameter must be callable.");
@@ -19,11 +39,19 @@ class Operation
         $this->callable = $callable;
     }
 
+    /**
+     * @param  null|array $arguments Must be serializable.
+     * @return bool
+     */
     public function expire($arguments = null) {
         $cacheKey = $this->makeCacheKey($this->name, $arguments);
         return $this->storage->removeItem($cacheKey);
     }
 
+    /**
+     * @param  null|array $arguments Must be serializable.
+     * @return mixed
+     */
     public function getValue($arguments = null) {
         $cacheKey = $this->makeCacheKey($this->name, $arguments);
         if(!$this->storage->hasItem($cacheKey)) {
@@ -32,6 +60,10 @@ class Operation
         return $this->storage->getItem($cacheKey);
     }
 
+    /**
+     * @param  null|array $arguments Must be serializable.
+     * @return void
+     */
     public function warm($arguments = null) {
         $callable = $this->callable;
         if(is_array($arguments)) {
@@ -45,6 +77,9 @@ class Operation
 
     /**
      * @todo  how to warn if any parameters are not serializable
+     * @param  string $name
+     * @param  null|array $arguments Must be serializable.
+     * @return mixed
      */
     protected function makeCacheKey($name, $arguments = null) {
         $keyParts = array($name);
